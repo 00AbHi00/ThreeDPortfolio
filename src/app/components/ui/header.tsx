f8c052c82c5d41ee4bca6e3905db48552eead86c
Abhi <silwalabhi2@gmail.com>
2025-04-04 00:46:16 +0545

    final commit of the day

--- src/app/components/ui/header.tsx
+++ src/app/components/ui/header.tsx
@@ -1,16 +1,31 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export const Header =()=>
-{
-    return(
-        <nav className="fixed z-10 flex h-15 items-center-safe w-screen justify-around  backdrop-blur-md">
-           <Link className="hover:bg-red-100/10 items-center p-3 h-full" href={"/happy"}>
-                Home
-           </Link>         
+export const Header = () => {
+  return (
+    <>
 
-           <Link className="hover:bg-red-100/10 items-center p-3 h-full" href={"/contact"}>
-                Contact Me
-           </Link>            
-        </nav>
-    )
-}
+      <nav
+        style={{ boxShadow: "-20px -20px 30px white" }}
+        className="fixed z-10 flex h-15 items-center-safe w-screen justify-around  backdrop-blur-md"
+      >
+        <Link
+          className="hover:bg-red-100/10 items-center p-3 h-full"
+          href={"/happy"}
+        >
+          Home
+        </Link>
+
+        <Link
+          className="hover:bg-red-100/10 items-center p-3 h-full"
+          href={"/contact"}
+        >
+          Contact Me
+        </Link>
+      </nav>
+      <a href="#main-content" tabIndex={99} 
+        className="z-10 fixed opacity-30 right-5 p-2  bottom-5 focus:opacity-100 cursor-pointer">
+            Skip to main Content
+       </a>
+    </>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,31 +9,34 @@ export default function Home() {
   const bodyRefrence = useRef<HTMLHeadingElement>(null);
   useGSAP(() => {
     const tl = gsap.timeline();
-    tl.fromTo(bodyRefrence.current,{
-      opacity:0.3,
-    }, {
-      duration: 3,
-      stagger:0.1,
-      opacity:1,
-      scrollTrigger: {
-        start:"40% top",
-        scrub: true,
-        pin:true,
-        trigger: titleRefrence.current,
-      },
-    }).to(titleRefrence.current,
+    tl.fromTo(
+      bodyRefrence.current,
       {
-        scale:0.5,
+        opacity: 0.3,
+        scale: 1,
+      },
+      {
+        duration: 3,
+        stagger: 0.1,
+        opacity: 1,
         scrollTrigger: {
-          start:"10% top",
+          start: "5% top",
           scrub: true,
           trigger: titleRefrence.current,
         },
       }
-    )
+    ).to(titleRefrence.current, {
+      scale: 0.8,
+      scrollTrigger: {
+        start: "10% top",
+        scrub: true,
+        trigger: titleRefrence.current,
+      },
+    });
   });
   const glitchyTextTitle = {
-    textShadow: "0.15em 0.15em 2px rgba(255,255,255,0.3), -0.15em -0.15em 2px rgba(0,0,255,0.3) ",
+    textShadow:
+      "0.15em 0.15em 2px rgba(255,255,255,0.3), -0.15em -0.15em 2px rgba(0,0,255,0.3) ",
   };
 
   return (
@@ -43,21 +46,30 @@ export default function Home() {
           <h1
             ref={titleRefrence}
             className=" pt-40 p-4 font-mono text-5xl text-cyan-400"
-            
           >
             Welcome To my 3d portfolio
           </h1>
-          <h2
-            className="text-xl "
-            style={glitchyTextTitle} 
-            ref={bodyRefrence}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque autem, architecto sapiente dolore dolor maxime maiores dolorum? Est nobis maiores officiis quis! Ut et, saepe officiis deserunt quia iure dolore?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque autem, architecto sapiente dolore dolor maxime maiores dolorum? Est nobis maiores officiis quis! Ut et, saepe officiis deserunt quia iure dolore?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque autem, architecto sapiente dolore dolor maxime maiores dolorum? Est nobis maiores officiis quis! Ut et, saepe officiis deserunt quia iure dolore?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque autem, architecto sapiente dolore dolor maxime maiores dolorum? Est nobis maiores officiis quis! Ut et, saepe officiis deserunt quia iure dolore?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque autem, architecto sapiente dolore dolor maxime maiores dolorum? Est nobis maiores officiis quis! Ut et, saepe officiis deserunt quia iure dolore?
-         
+          <h2 className="text-xl  " style={glitchyTextTitle} ref={bodyRefrence}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+            autem, architecto sapiente dolore dolor maxime maiores dolorum? Est
+            nobis maiores officiis quis! Ut et, saepe officiis deserunt quia
+            iure dolore? Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Atque autem, architecto sapiente dolore dolor maxime maiores
+            dolorum? Est nobis maiores officiis quis! Ut et, saepe officiis
+            deserunt quia iure dolore? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Atque autem, architecto sapiente dolore dolor
+            maxime maiores dolorum? Est nobis maiores officiis quis! Ut et,
+            saepe officiis deserunt quia iure dolore? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Atque autem, architecto sapiente
+            dolore dolor maxime maiores dolorum? Est nobis maiores officiis
+            quis! Ut et, saepe officiis deserunt quia iure dolore? Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Atque autem, architecto
+            sapiente dolore dolor maxime maiores dolorum? Est nobis maiores
+            officiis quis! Ut et, saepe officiis deserunt quia iure dolore?
           </h2>
+        </div>
+        <div id="main-content" className="pt-[100px] -mt-[100px]">
+          <h1>This is main content</h1>
         </div>
       </div>
     </>
