781057c7eb4efd88d5584cf3ac519c17dd040959
Abhi <silwalabhi2@gmail.com>
2025-04-15 23:15:27 +0545

    added new content page with some styling still need more improvement. Features to add 1. Clear idea on where this should go. 2. Desgin in blender and convert to code. 3. Proper documentation of this site

---
+++ src/app/(pages)/contact/page.tsx
@@ -0,0 +1,14 @@
+export default function FunctionName() {
+  return (
+    <div className="grid min-h-screen place-items-center">
+      <span className="p-4 shadow-sm shadow-white rounded-e-2xl">
+        Contact  Me
+        <div>
+            Phone: 9813011562
+        </div>
+      </span>
+        
+
+    </div>
+  );
+}
