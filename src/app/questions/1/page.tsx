2b2cebc85ef3af6be4f916835a2e7263fca5517b
Abhi <silwalabhi2@gmail.com>
2025-05-04 22:25:52 +0545

    Adding functionalities for testing skills

---
+++ src/app/questions/1/page.tsx
@@ -0,0 +1,12 @@
+
+export default function FunctionName() {
+  return (
+    <div className="grid h-screen place-items-center ">
+        <div>
+          <input 
+          className="p-3 text-2xl outline focus:outline-red-900"
+          type="search" name="" id="" />              
+        </div>
+    </div>
+  );
+}
