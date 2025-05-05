bb5c26173df65361cf2e2a13a143cec03aba74e2
Abhi <silwalabhi2@gmail.com>
2025-05-05 11:15:17 +0545

    successfully debounce using useDebounse

--- src/app/questions/1/page.tsx
+++ src/app/questions/1/page.tsx
@@ -1,12 +1,31 @@
-
+'use client'
+import  { useState,useEffect, useRef } from "react";
+import useDebounce from "./useDebounce";
 export default function FunctionName() {
-  return (
+    const InputRef=useRef<HTMLInputElement>(null)
+    const [input, setInput]= useState('');
+    const debouncedInp=useDebounce(input,1000)
+    const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>
+    {
+      if(InputRef.current)
+      {
+        setInput(InputRef.current?.value)
+      }
+    }
+
+    return (
     <div className="grid h-screen place-items-center ">
         <div>
           <input 
-          className="p-3 text-2xl outline focus:outline-red-900"
-          type="search" name="" id="" />              
+            ref={InputRef}
+            onChange={handleChange}
+            className="p-3 text-2xl outline focus:outline-red-900"
+            type="search" name="" id="" />              
         </div>
+        <span className="">
+          {input}
+         De: {debouncedInp}
+        </span>
     </div>
   );
 }
